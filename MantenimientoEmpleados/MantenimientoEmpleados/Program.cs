ad0a7fa6dc280014536b466c24b2bbb14d46e6df
ric7x <ricardorosario89@gmail.com>
2016-11-22 13:19:52 -0400

    cargando menu principal

--- MantenimientoEmpleados/MantenimientoEmpleados/Program.cs
+++ MantenimientoEmpleados/MantenimientoEmpleados/Program.cs
@@ -16,7 +16,7 @@ namespace MantenimientoEmpleados
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMenu());
         }
     }
 }
