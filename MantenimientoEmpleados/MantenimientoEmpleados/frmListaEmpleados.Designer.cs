da8c2cf4eb6a0e06e8f9fe67f9340ac312442002
ric7x <ricardorosario89@gmail.com>
2016-11-29 23:57:49 -0400

    cargando datos del grid a los texbox de otros formularios

--- MantenimientoEmpleados/MantenimientoEmpleados/frmListaEmpleados.Designer.cs
+++ MantenimientoEmpleados/MantenimientoEmpleados/frmListaEmpleados.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,10 +42,12 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 130);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1072, 489);
+            this.dataGridView1.Size = new System.Drawing.Size(1072, 371);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDoubleClick);
             // 
             // button1
             // 
@@ -55,11 +59,30 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // frmListaEmpleados
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1104, 576);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "frmListaEmpleados";
@@ -68,6 +91,7 @@
             this.Load += new System.EventHandler(this.frmListaEmpleados_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,5 +99,7 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- MantenimientoEmpleados/MantenimientoEmpleados/frmListaEmpleados.cs
+++ MantenimientoEmpleados/MantenimientoEmpleados/frmListaEmpleados.cs
@@ -19,6 +19,14 @@ namespace MantenimientoEmpleados
             InitializeComponent();
         }
 
+        public string nombre;
+        public string apellido;
+        public string sexo;
+        public string fechaNacimiento;
+        public string estatus;
+        public string salario;
+        public string fechaIngreso;
+
         private void frmListaEmpleados_Load(object sender, EventArgs e)
         {
             cargarEmpleados();
@@ -40,6 +48,48 @@ namespace MantenimientoEmpleados
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string con = ConfigurationManager.ConnectionStrings["cnx"].ToString();
+            SqlConnection conexion = new SqlConnection(con);
+
+            conexion.Open();
+            SqlDataAdapter da = new SqlDataAdapter("select *from empleado where nombre='"+textBox1.Text+"'", conexion);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "empleado");
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "empleado";
+            conexion.Close();
+        }
+
+        private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        public void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            nombre = dataGridView1.Rows[e.RowIndex].Cells["nombre"].Value.ToString();
+            apellido = dataGridView1.Rows[e.RowIndex].Cells["apellido"].Value.ToString();
+            sexo = dataGridView1.Rows[e.RowIndex].Cells["sexo"].Value.ToString();
+            fechaNacimiento = dataGridView1.Rows[e.RowIndex].Cells["fechaNacimiento"].Value.ToString();
+            fechaIngreso = dataGridView1.Rows[e.RowIndex].Cells["fechaIngreso"].Value.ToString();
+            salario = dataGridView1.Rows[e.RowIndex].Cells["salario"].Value.ToString();
+            estatus = dataGridView1.Rows[e.RowIndex].Cells["estatus"].Value.ToString();
+
+            Form1 f1 = new Form1();
+            f1.txtNombre.Text = nombre;
+            f1.txtApellido.Text = apellido;
+            f1.txtEstatus.Text = estatus;
+            f1.dtFechaNacimiento.Text = fechaNacimiento;
+            f1.cmbSexo.Text = sexo;
+            f1.txtSalario.Text = salario;
+
+            f1.Show();
+            
+            
         }
     }
 }
