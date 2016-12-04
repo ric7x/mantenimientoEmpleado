b7f8c4fe53eb4cb649c2c8a9a15beed721e5c209
ric7x <ricardorosario89@gmail.com>
2016-12-03 20:03:34 -0400

    reporte

---
+++ MantenimientoEmpleados/MantenimientoEmpleados/frmReporte.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace MantenimientoEmpleados
+{
+    partial class frmReporte
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1004, 734);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generar Reporte";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.CachedPageNumberPerDoc = 10;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(12, 12);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1124, 709);
+            this.crystalReportViewer1.TabIndex = 1;
+            // 
+            // frmReporte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1148, 780);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Controls.Add(this.button1);
+            this.Name = "frmReporte";
+            this.Text = "frmReporte";
+            this.Load += new System.EventHandler(this.frmReporte_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+
+    }
+}
---
+++ MantenimientoEmpleados/MantenimientoEmpleados/frmReporte.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace MantenimientoEmpleados
+{
+    public partial class frmReporte : Form
+    {
+        public frmReporte()
+        {
+            InitializeComponent();
+        }
+
+        private void frmReporte_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReportDocument crystalrpt = new ReportDocument();
+            crystalrpt.Load(@"C:\Users\TheHangover\Source\Repos\NewRepo\MantenimientoEmpleados\MantenimientoEmpleados\CrystalReport1.rpt");
+            crystalReportViewer1.ReportSource = crystalrpt;
+            crystalReportViewer1.Refresh();
+
+        }
+    }
+}
