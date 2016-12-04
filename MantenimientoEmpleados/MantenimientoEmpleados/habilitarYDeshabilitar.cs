f4208a394b2d4857097e9862fd60f9dba91ddc5d
ric7x <ricardorosario89@gmail.com>
2016-12-03 20:04:00 -0400

    habilitar texbox y deshabilitar

---
+++ MantenimientoEmpleados/MantenimientoEmpleados/habilitarYDeshabilitar.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MantenimientoEmpleados
+{
+    class habilitarYDeshabilitar
+    {
+
+        public void deshabilitar(Control control)
+        {
+            foreach (var txt in control.Controls)
+            {
+                if (txt is TextBox)
+                {
+                    ((TextBox)txt).Enabled=false;
+                }                   
+        
+        else if(txt is ComboBox)
+            {
+                ((ComboBox)txt).Enabled = false;
+            }
+                else if (txt is DateTimePicker)
+                {
+                    ((DateTimePicker)txt).Enabled = false;
+                }
+
+
+
+            }
+        }
+
+
+        public void habilitar(Control control)
+        {
+            foreach (var txt in control.Controls)
+            {
+                if (txt is TextBox)
+                {
+                    ((TextBox)txt).Enabled = true;
+                }
+
+                else if (txt is ComboBox)
+                {
+                    ((ComboBox)txt).Enabled = true;
+                }
+                else if (txt is DateTimePicker)
+                {
+                    ((DateTimePicker)txt).Enabled = true;
+                }
+
+
+
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
