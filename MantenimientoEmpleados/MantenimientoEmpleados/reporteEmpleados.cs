2a94f20573b4d47052dd485f228a3ba73ea22586
ric7x <ricardorosario89@gmail.com>
2016-12-08 01:38:36 -0400

    reporte de calculode nomina

---
+++ MantenimientoEmpleados/MantenimientoEmpleados/reporteEmpleados.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace MantenimientoEmpleados
+{
+    public partial class SueldoEmpleados : Form
+    {
+        public SueldoEmpleados()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReportDocument crysta2rpt = new ReportDocument();
+            crysta2rpt.Load(@"C:\Users\TheHangover\Source\Repos\NewRepo\MantenimientoEmpleados\MantenimientoEmpleados\CrystalReport2.rpt");
+            crystalReportViewer1.ReportSource = crysta2rpt;
+            crystalReportViewer1.Refresh();
+        }
+
+        private void SueldoEmpleados_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
